34de986ab62301cf5f333b983861c51fc4c579bd
negativeInteger <139580532+INFERNO1211@users.noreply.github.com>
2025-03-27 21:46:49 +0530

    clickable grid updated

--- src/components/Matrix.jsx
+++ src/components/Matrix.jsx
@@ -1,20 +1,20 @@
 import { useState } from "react";
 
 function Matrix() {
-  // Create 3 X 3 grid (9 boxes)
+  // Total Boxes in the grid
   const totalBoxes = 9;
 
-  // Store box colors (initially all white)
+  // Store colors of each box with default as white
   const [colors, setColors] = useState(Array(totalBoxes).fill('white'));
 
-  // Track which boxes were clicked
+  // Track clicked boxes
   const [clickedBoxes, setClickedBoxes] = useState([]);
 
   // Handle Box clicks
   function handleClick(index) {
     if(colors[index] !== 'white') return; // Ignore already clicked boxes
 
-    // Change clicked box to green
+    // Change color of clicked box to green
     const newColors = [...colors];
     newColors[index] = 'green';
     setColors(newColors);
@@ -23,7 +23,7 @@ function Matrix() {
     const newClickedBoxes = [...clickedBoxes, index]
     setClickedBoxes(newClickedBoxes);
 
-    // If this was the last click (9th box), change all to orange in order
+    // Change color of all to orange in the order they were clicked if last box was clicked
     if(newClickedBoxes.length === totalBoxes) {
       newClickedBoxes.forEach((pos, i) => {
         setTimeout(() => {
@@ -38,7 +38,9 @@ function Matrix() {
   }
 
   return (
-    <>    
+    <>
+    {/* Map over colors to render each box in the matrix  */}
+    
     <div style={{ display:'grid', gridTemplateColumns:'repeat(3, 100px)', gap: '5px' }}>
       {
         colors.map((color, index) => (
